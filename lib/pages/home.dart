e3f539f8828031d07d67f5d767e4313f5df09758
LFPires1993 <lfpires1993@gmail.com>
2024-03-26 20:26:20 -0300

    Create and stilyze some widgets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,8 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Lista de tarefas'),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Digite sua tarefa',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                child: Icon(Icons.add),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
