7efde00bfc471eea9e02471d369f67a80c29351e
LFPires1993 <lfpires1993@gmail.com>
2024-04-01 19:31:16 -0300

    Create some structures

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  String textOnField = '';
+
+  final _buttonsStyle = ElevatedButton.styleFrom(
+    backgroundColor: const Color(0xff00d7f3),
+    padding: const EdgeInsets.all(17),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -13,23 +23,76 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text('Lista de tarefas'),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Digite sua tarefa',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(),
-                  ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'Lista de tarefas',
                 ),
               ),
-              ElevatedButton(
-                onPressed: () => {},
-                child: Icon(Icons.add),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: changed,
+                      decoration: InputDecoration(
+                        labelText: 'Adicione uma tarefa',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: pressButton,
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    style: _buttonsStyle,
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              ListView(
+                children: [
+
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possuí x tarefas pendentes',
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Limpar tudo',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: _buttonsStyle,
+                  )
+                ],
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  changed(String text) {
+    textOnField = text;
+  }
+
+  pressButton() {
+    print(textOnField);
   }
 }
